a54624eb8a6db23d96b120d6f4fcf70279db0b44
Bruno-MdeMaria <brunomartinsdemaria@gmail.com>
2022-07-09 17:36:59 -0300

    Create main53final.py

---
+++ main53final.py
@@ -0,0 +1,24 @@
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n"))
+nr_symbols =int(input(f"How many symbols would you like?\n"))
+nr_numbers =int(input(f"How many numbers would you like?\n"))
+
+password = ""
+for car in range(1, nr_letters +1): # pega o numero de letra escolhida para gerar quantas vezes o loop será feito: Ex. 4? de 1 a numero escolhido +1 = 4(0,1,2,3,4)
+    password += random.choice(letters) # variavel password recebe e acrescenta (+=) uma letra aleatoria da lista(randon.choice) em cada loop.
+
+for car in range(1, nr_symbols  +1):
+    password += random.choice(symbols)
+
+for car in range(1, nr_numbers +1):
+    password += random.choice(numbers)
+
+#print(password)# importante = imprmi fora do loop para pegar a variavel somada.
+print(f"Here is your password: {password}")
+print(f"Aqui está a sua senha: {password}")
